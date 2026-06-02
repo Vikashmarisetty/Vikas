e487678cca68dcafe34a28f6cbc3018e8fc036a4
Vikash marisetty <vikashmarisetty@gmail.com>
2026-06-02 10:17:09 +0530

    Add files via upload

---
+++ greatest of three numbers .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter values of a,b,c; ");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+printf("%d is greater",a);
+if(a<b)
+printf("%d is greater ",b);
+else
+printf("%d is greater",c);
+}
+    
